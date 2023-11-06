b0902b264e518b0b939972824a92e1af93e58ed7
chandiryn <apps.chandiran@gmail.com>
2023-11-07 01:51:26 +0530

    default-01

--- Git-Repo-Files/terraform-manifests/c1-versions.tf
+++ Git-Repo-Files/terraform-manifests/c1-versions.tf
@@ -15,6 +15,13 @@ terraform {
       version = ">= 3.0"
     }    
   }
+# Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+    resource_group_name   = "DevOps-Terraform-IaC23-RG"
+    storage_account_name  = "devopsterraformstorage23"
+    container_name        = "devopsterraformstoragecontainer23"
+    key                   = "project-1-eastus2-terraform.tfstate"
+  }  
 }
 
 # Provider Block
@@ -23,4 +30,3 @@ provider "azurerm" {
 }
 
 
-#test
